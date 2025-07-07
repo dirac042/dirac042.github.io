4fbb55756045421c708be2b23160ee7c8db685d4
dirac042@gmail.com <dirac042@gmail.com>
2025-07-07 21:38:34 +0900

    New Blog Post on 2025-07-07 21:38:34

--- updateblog.sh
+++ updateblog.sh
@@ -85,9 +85,9 @@ else
 fi
 
 # Step 7: Push all changes to the main branch
-echo "Deploying to GitHub Main..."
-if ! git push origin main; then
-  echo "Failed to push to main branch."
+echo "Deploying to GitHub Master..."
+if ! git push origin master; then
+  echo "Failed to push to master branch."
   exit 1
 fi
 
