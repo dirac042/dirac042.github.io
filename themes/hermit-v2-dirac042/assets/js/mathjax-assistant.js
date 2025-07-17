e134d1f5680d815f76fe719c9edcaa8473c5ae9f
dirac042@gmail.com <dirac042@gmail.com>
2025-07-17 14:25:38 +0900

    test

--- themes/hermit-v2-dirac042/assets/js/mathjax-assistant.js
+++ themes/hermit-v2-dirac042/assets/js/mathjax-assistant.js
@@ -1,12 +1,19 @@
 MathJax = {
   tex: {
-    displayMath: [['\\[', '\\]'], ['$$', '$$']],  // block
-    inlineMath: [['\\(', '\\)']]                  // inline
-      // processEscapes: true,
-      // processEnvironments: true,
-  }
+    displayMath: [
+      ["\\[", "\\]"],
+      ["$$", "$$"],
+    ], // block
+    inlineMath: [
+      ["\\(", "\\)"],
+      ["$", "$"],
+    ], // inline
+    // processEscapes: true,
+    // processEnvironments: true,
+  },
   // options: {
   //     skipHtmlTags: ['script', 'noscript', 'style', 'textarea', 'pre'],
   //     enableMenu: false
   // }
 };
+
